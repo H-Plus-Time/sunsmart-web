e8b96edd6b275a5cf9914e6cabfcbe0283ad6b33
H-Plus-Time <nicholas.roberts.au@gmail.com>
2017-01-05 23:01:47 +1100

    Initial commit - generated ES6 build, early work on Firebase cloud messaging

---
+++ fcm-worker-ext.js
@@ -0,0 +1,10 @@
+var window = self;
+
+importScripts(
+  '/bower_components/firebase/firebase-app.js',
+  '/bower_components/firebase/firebase-messaging.js'
+);
+
+firebase.initializeApp({
+  messagingSenderId: '97269898080'
+});
